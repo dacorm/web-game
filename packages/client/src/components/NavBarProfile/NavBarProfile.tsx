fc73ec2642a46d29c21de5d43962484e52412895
shmelevfm <shmelfrol@gmail.com>
2022-12-23 19:16:43 +0300

    add DropDownMenu at NavBarProfile

--- packages/client/src/components/NavBarProfile/NavBarProfile.tsx
+++ packages/client/src/components/NavBarProfile/NavBarProfile.tsx
@@ -1,16 +1,23 @@
 import styles from './NavBarProfile.module.css';
-import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react'
 import {NavBarProfileProps } from './NavBarProfile.types';
 import { memo } from 'react';
+import { NavBarProfileDropMenu } from '../NavBarProfileDropMenu/NavBarProfileDropMenu'
 
 export const  NavBarProfile=memo(({ userLogo, userName }: NavBarProfileProps)=> {
+ const [drop, Setdrop]=useState(false)
+
+  const clickHandler=()=>{
+   console.log("drop", drop)
+    Setdrop(!drop)
+  }
 
   return (
     <div className={styles.navbarUser}>
-      <Link to={'/profile'}>
-        <img src={userLogo} className={styles.userLogo} alt={userName} /> {userName}
-      </Link>
+     <div onClick={clickHandler}>
+       <img src={userLogo} className={styles.userLogo} alt={userName} /> {userName}
+     </div>
+        <NavBarProfileDropMenu drop={drop} onClick={clickHandler}/>
     </div>
   );
 });
---
+++ packages/client/src/components/NavBarProfileDropMenu/NavBarProfileDropMenu.tsx
@@ -0,0 +1,26 @@
+import styles from './NavBarProfileDropMenu.module.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+interface NavBarProfileDropMenuProps {
+  drop: boolean
+  onClick: () => void
+}
+
+
+export function NavBarProfileDropMenu({ drop, onClick }: NavBarProfileDropMenuProps) {
+
+  const style = drop ? `${styles.dropdown}` : `${styles.dropdown} ${styles.dropdownActive}`
+
+  return (
+    <ul className={style} onClick={onClick}>
+      <li className={styles.dropdownItem}>
+        <Link to={'/profile'} >
+          Профиль
+        </Link>
+      </li>
+      <li>Выйти</li>
+    </ul>
+  )
+
+}
