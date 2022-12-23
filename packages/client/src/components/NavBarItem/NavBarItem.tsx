67b983537cd88a65af6f75e26fe4050f9890a73f
shmelevfm <shmelfrol@gmail.com>
2022-12-23 13:04:44 +0300

    isActive in Navlink

--- packages/client/src/components/NavBarItem/NavBarItem.tsx
+++ packages/client/src/components/NavBarItem/NavBarItem.tsx
@@ -2,13 +2,12 @@ import { NavLink } from 'react-router-dom'
 import styles from './NavBarItem.module.css'
 import { NavbarItemProps } from './NavBarItem.types'
 
-export function NavBarItem({ path, title }: NavbarItemProps) {
+const isActive=({ isActive }:{isActive:boolean}) => isActive ? `${styles.menuItem} ${styles.menuItemActive}` : `${styles.menuItem}`
 
+export function NavBarItem({ path, title }: NavbarItemProps) {
   return (
     <li>
-      <NavLink
-        to={path}
-        className={({ isActive }) => isActive ? `${styles.menuItem} ${styles.menuItemActive}` : `${styles.menuItem}`}>
+      <NavLink to={path} className={isActive}>
         {title}
       </NavLink>
     </li>
