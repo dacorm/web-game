51ef6117ee5a43bc5739123916734e2e9ce2bf34
shmelevfm <shmelfrol@gmail.com>
2022-12-23 22:01:28 +0300

    add CONSTANT ROUTES.

--- packages/client/src/components/NavBarLogo/NavBarLogo.tsx
+++ packages/client/src/components/NavBarLogo/NavBarLogo.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import logo from '../../assets/img/MainLoGo.png'
 import styles from './NavBarLogo.module.css'
 import { memo } from 'react'
-export const NavBarLogo = memo(()=> {
+
+export const NavBarLogo = memo(() => {
 
   return (
     <div>
       <img className={styles.mainLogo} src={logo} alt='Monopoly' />
-      Monopoly Game</div>
+      Monopoly Game
+    </div>
   )
 })
