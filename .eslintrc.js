22ecd5aa44fcace6998e9c4f7067a748235babd5
benzobak83 <vitanaolord@gmail.com>
2022-12-28 20:31:25 +0300

    fix: try to fix unsuccessfull tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,6 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
-<<<<<<< HEAD
     '@typescript-eslint/no-explicit-any': ['off'],
-=======
->>>>>>> main
   },
 }
