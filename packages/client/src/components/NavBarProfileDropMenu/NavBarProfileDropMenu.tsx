6f916e5be604f775d85de8280849c49ed8bce72f
sergey.antonov <sergey.antonov@nedra.digital>
2022-12-28 05:40:20 +0300

    feat(NOT-17): Рефактор роутера
    * Добавлен routerSuspense hof
    * Немного косметики
    
    Closes NOT-17

--- packages/client/src/components/NavBarProfileDropMenu/NavBarProfileDropMenu.tsx
+++ packages/client/src/components/NavBarProfileDropMenu/NavBarProfileDropMenu.tsx
@@ -1,27 +1,27 @@
 import styles from './NavBarProfileDropMenu.module.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { ROUTES } from '../../constants'
+import { ROUTES } from '../../layout/RouterLayout/RouterConst'
 
 interface NavBarProfileDropMenuProps {
   drop: boolean
   onClick: () => void
 }
 
-
-export function NavBarProfileDropMenu({ drop, onClick }: NavBarProfileDropMenuProps) {
-
-  const style = drop ? `${styles.dropdown} ${styles.dropdownActive}` : `${styles.dropdown}`
+export function NavBarProfileDropMenu({
+  drop,
+  onClick,
+}: NavBarProfileDropMenuProps) {
+  const style = drop
+    ? `${styles.dropdown} ${styles.dropdownActive}`
+    : `${styles.dropdown}`
 
   return (
     <ul className={style} onClick={onClick}>
       <li className={styles.dropdownItem}>
-        <Link to={ROUTES.PROFILE} >
-          Профиль
-        </Link>
+        <Link to={ROUTES.PROFILE}>Профиль</Link>
       </li>
       <li>Выйти</li>
     </ul>
   )
-
 }
