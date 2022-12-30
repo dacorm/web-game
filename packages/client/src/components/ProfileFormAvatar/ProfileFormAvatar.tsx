bc0bb4cb22c864819e8042e558441c99f4670e03
shmelevfm <shmelfrol@gmail.com>
2022-12-30 12:15:30 +0300

    changeHandler in choose File

--- packages/client/src/components/ProfileFormAvatar/ProfileFormAvatar.tsx
+++ packages/client/src/components/ProfileFormAvatar/ProfileFormAvatar.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ButtonTheme } from '../../shared/ui/Button/Button.types'
 import styles from './ProfileFormAvatar.module.css'
 import Button from '../../shared/ui/Button'
 
 export function ProfileFormAvatar(){
+  const [file, Setfile]=useState('')
+  const choosedFiles=file ? `Выран файл - ${file}`: ''
+
+  const changeHandler=(e:React.FormEvent<HTMLInputElement>)=>{
+    const target =e.target as HTMLInputElement
+    if(target.files)
+    if(target.files[0]){
+      Setfile(target.files[0].name)
+    }
+  }
+
   return(
     <form className={styles.profileForm}>
       <label className={styles.label}>
         Выберите Аватар
-        <input className={styles.fileInput} type={"file"}/>
+        <input className={styles.fileInput} type={"file"} onChange={changeHandler}/>
       </label>
-
+      {choosedFiles && <p className={styles.choosedFiles}>{choosedFiles}</p>}
       <Button theme={ButtonTheme.GREEN} type='submit' className={styles.button}>Сохранить</Button>
     </form>
   )
