b9bccf8ab180f099229f48469922915718cefc3e
benzobak83 <vitanaolord@gmail.com>
2023-01-04 01:23:03 +0300

    fix: edited lefthook.yml

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     ],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
+    "linebreak-style": 0,
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'react/require-default-props': 'off',
