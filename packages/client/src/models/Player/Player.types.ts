1211edfbd9cecda8d8b7166b29fb20f8bbb9b327
benzobak83 <vitanaolord@gmail.com>
2023-01-12 17:18:38 +0300

    fix: changed PlayerProps interface

--- packages/client/src/models/Player/Player.types.ts
+++ packages/client/src/models/Player/Player.types.ts
@@ -1,7 +1,7 @@
 import { Canvas } from '../../core/Canvas/helpers/Canvas';
 
 export interface PlayerProps {
-    canvas: Canvas|undefined
+    canvas: Canvas
     userId: number
     displayName: string
 }
