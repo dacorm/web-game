b8fb0955f4340e1fdd7448d11800ca5775624d88
enc2b1q <93642807+enc2b1q@users.noreply.github.com>
2023-02-01 01:26:20 +0300

    Drag and Drop API at avatar changing + image type check

--- packages/client/src/components/Profile/ProfileFormAvatar/ProfileFormAvatar.tsx
+++ packages/client/src/components/Profile/ProfileFormAvatar/ProfileFormAvatar.tsx
@@ -1,4 +1,5 @@
-import React, { FormEvent, useState } from 'react';
+import React, { DragEvent, FormEvent, useState } from 'react';
+import classNames from 'classnames';
 import { ButtonSize, ButtonTheme } from '../../../shared/ui/Button/Button.types';
 import styles from './ProfileFormAvatar.module.css';
 import Button from '../../../shared/ui/Button';
@@ -7,12 +8,31 @@ import { useActions } from '../../../hooks/useActions';
 
 export function ProfileFormAvatar() {
     const [file, setFile] = useState<File | null>(null);
+    const [error, setError] = useState<boolean>(false);
     const { setUserAvatarThunk } = useActions();
+
+    const isImage = (theFile: File) => {
+        const fileType: string = theFile.type;
+        console.log('fileType', fileType);
+        const regex = /image\//;
+        return fileType.match(regex);
+    };
+
+    const putImage = (tempFile: File) => {
+        if (isImage(tempFile)) {
+            setError(false);
+            setFile(tempFile);
+        } else {
+            setError(true);
+        }
+    };
+
     const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement;
         if (target.files) {
             if (target.files[0]) {
-                setFile(target.files[0]);
+                const tempFile: File = target.files[0];
+                putImage(tempFile);
             }
         }
     };
@@ -21,22 +41,50 @@ export function ProfileFormAvatar() {
         e.preventDefault();
         if (file) {
             setUserAvatarThunk(file);
+            setFile(null);
+        }
+    };
+
+    const handleOndragOver = (e: DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        e.dataTransfer.dropEffect = 'copy';
+    };
+
+    const handleOndrop = (e: DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const tempFile = e.dataTransfer.files[0];
+        if (tempFile) {
+            putImage(tempFile);
         }
     };
 
     return (
         <form className={styles.profileForm} onSubmit={btnClickHandler}>
+
             <label className={styles.label} htmlFor="avatar">
+
                 Выберите фотo
                 <input
                     className={styles.fileInput}
                     type="file"
                     name="avatar"
                     id="avatar"
+                    accept="image/*"
                     onChange={changeHandler}
+                    hidden
                 />
-            </label>
 
+            </label>
+            <div
+                className={classNames(styles.dragZone, file ? styles.active : '')}
+                onDragOver={handleOndragOver}
+                onDrop={handleOndrop}
+            >
+                <span>или перетяните на поле</span>
+            </div>
+            {error && <p className={styles.error}>{error ? 'Выбран ошибочный файл - необходимо изображение для загрузки' : ''}</p>}
             {file && <p className={styles.choosedFiles}>{file ? `Выбран файл - ${file.name}` : ''}</p>}
             {file && (
                 <Button
