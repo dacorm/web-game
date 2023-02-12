50d6e16c7b5b8bfc855e6d220ac06331ebb58d50
enc2b1q <93642807+enc2b1q@users.noreply.github.com>
2023-02-12 21:21:42 +0300

    oauth v1

---
+++ packages/client/src/api/apiTypes.ts
@@ -0,0 +1,3 @@
+export interface OAuthGetServiceResponse {
+  service_id: string
+}
--- packages/client/src/redux/types/userReducer.types.ts
+++ packages/client/src/redux/types/userReducer.types.ts
@@ -21,13 +21,18 @@ SET_AVATAR='SET_AVATAR',
 LOGIN_ERROR='LOGIN_ERROR'
 }
 
+export const redirectURI = 'http://localhost:3000';
+// export const redirectURI = 'http://localhost/login:3000';  //только http://localhost:3000 зарегистрирован как callback_url в практикуме
+
 export enum UserURL {
   BASE_AVATAR_URL='https://ya-praktikum.tech/api/v2/resources/',
   AVATAR='https://ya-praktikum.tech/api/v2/user/profile/avatar',
   SIGNUP='https://ya-praktikum.tech/api/v2/auth/signup',
   LOGOUT='https://ya-praktikum.tech/api/v2/auth/logout',
   USERINFO='https://ya-praktikum.tech/api/v2/auth/user',
-  LOGIN='https://ya-praktikum.tech/api/v2/auth/signin'
+  LOGIN='https://ya-praktikum.tech/api/v2/auth/signin',
+  OAUTH_SERVICE_ID='https://ya-praktikum.tech/api/v2/oauth/yandex/service-id',
+  OAUTH_LOGIN='https://ya-praktikum.tech/api/v2/oauth/yandex'
 }
 
 type TUserActionlogout = {
